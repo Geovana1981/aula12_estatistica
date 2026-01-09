c9c60c16d8a063c490e965733fc5b6b84394abde
Geovana1981 <gegepiedade@gmail.com>
2026-01-08 21:19:18 -0300

    Series

--- aula_01/exemplo02.py
+++ aula_01/exemplo02.py
@@ -3,4 +3,6 @@ import pandas as pd
 produtos = ["Notebook", "Smartphone", "Tablet", "Smartwatch", "Câmera", "Notebook"]
 qtds_estoque = [16, 28, 19, 13, 26, 9]
 
-serie_estoque = 
+serie_estoque = pd.Series(qtds_estoque, index=produtos)
+print(serie_estoque)
+print(serie_estoque["Notebook"])
